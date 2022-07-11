fc180a5eedba5bae50a0d060732ee207b0e370b0
Mark Talent <mtalent@live.com>
2022-07-11 05:44:49 -0400

    it works using string builder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,78 @@ import java.io.File
 
 
 fun main() {
-    val fileName = "data.txt"
-    var file = File(fileName)
-    file = writeLines(file)
+    val file = File("data.txt").also {
+        writeLines(it)
+    }
     val returnedList = readDataTxt(file)
     val results = buildObjects(returnedList)
-    val resString = doLogic(results)
-    println(resString)
+    val counts = doLogic(results)
+    formatStrings(counts).also {
+        println(it)
+    }
 }
 
-fun doLogic(results: List<Result>): String {
+fun formatStrings(counts: Counts): String {
 
+    val builder = StringBuilder()
+
+    val stringClearance = if (counts.countClearancePrice < 2) {
+        String.format("Clearance Price : %d products  @ %.2f"
+            ,counts.clearanceHigh)
+    }else {
+        String.format("Clearance Price : %d products  @ %.2f - %.2f"
+            ,counts.clearanceHigh, counts.clearanceLow)
+    }
+    val stringNormal = if (counts.countRegPrice < 2) {
+        String.format("Regular Price : %d products  @ %.2f"
+            ,counts.regularPriceHigh)
+    }else {
+        String.format("Regular Price : %d products  @ %.2f - %.2f"
+            ,counts.regularPriceHigh, counts.regularPriceLow)
+    }
+    val stringCart = String.format("Price in Cart", counts.priceInCartCount)
+
+    if (counts.countRegPrice > counts.countClearancePrice) {
+        if (counts.priceInCartCount > counts.countRegPrice) {
+            builder.append(stringCart)
+                .appendLine(stringNormal)
+                .appendLine(stringClearance)
+        } else {
+            if (counts.priceInCartCount > counts.countClearancePrice) {
+                builder.append(stringNormal)
+                    .appendLine(stringCart)
+                    .appendLine(stringClearance)
+            } else {
+                builder.append(stringNormal)
+                    .appendLine(stringClearance)
+                    .appendLine(stringCart)
+            }
+        }
+
+
+    }else {
+        if (counts.priceInCartCount > counts.countClearancePrice) {
+            builder.append(stringCart)
+                .appendLine(stringClearance)
+                .appendLine(stringNormal)
+        } else {
+            if (counts.priceInCartCount > counts.countRegPrice) {
+                builder.append(stringClearance)
+                    .appendLine(stringCart)
+                    .appendLine(stringNormal)
+            } else {
+                builder.append(stringClearance)
+                    .appendLine(stringNormal)
+                    .appendLine(stringCart)
+            }
+        }
+    }
+
+    return  builder.toString()
+}
+
+fun doLogic(results: List<Result>): Counts {
+/*
     var countRegPrice = 0
     var countClearancePrice = 0
     var clearanceHigh = 0.00
@@ -20,63 +81,43 @@ fun doLogic(results: List<Result>): String {
     var priceInCartCount = 0
     var regularPriceHigh = 0.00
     var regularPriceLow = 0.00
-
+*/
+    val count = Counts()
 
     results.forEach {
 
        if (it.quantity!! > 3) {
            if (it.regularPrice == it.clearancePrice) {
 
-               if (regularPriceHigh == 0.00) {
-                   regularPriceHigh = it.regularPrice!!
-                   regularPriceLow = it.regularPrice
+               if (count.regularPriceHigh == 0.00) {
+                   count.regularPriceHigh = it.regularPrice!!
+                   count.regularPriceLow = it.regularPrice
                }
-               countRegPrice++
-               if (regularPriceHigh < it.regularPrice!!)
-                   regularPriceHigh = it.regularPrice
-               if (regularPriceLow > it.regularPrice)
-                   regularPriceLow = it.regularPrice
+               count.countRegPrice++
+               if (count.regularPriceHigh < it.regularPrice!!)
+                   count.regularPriceHigh = it.regularPrice
+               if (count.regularPriceLow > it.regularPrice)
+                   count.regularPriceLow = it.regularPrice
                if (it.cart == true)
-                   priceInCartCount++
+                   count.priceInCartCount++
 
            }else {
-               if (clearanceHigh == 0.00) {
-                   clearanceHigh = it.clearancePrice!!
-                   clearanceLow = it.clearancePrice
+               if (count.clearanceHigh == 0.00) {
+                   count.clearanceHigh = it.clearancePrice!!
+                   count.clearanceLow = it.clearancePrice
                }
-               countClearancePrice++
-               if (clearanceHigh < it.clearancePrice!!)
-                   clearanceHigh = it.clearancePrice
-               if (clearanceLow > it.clearancePrice)
-                   clearanceLow = it.clearancePrice
+               count.countClearancePrice++
+               if (count.clearanceHigh < it.clearancePrice!!)
+                   count.clearanceHigh = it.clearancePrice
+               if (count.clearanceLow > it.clearancePrice)
+                   count.clearanceLow = it.clearancePrice
                if (it.cart == true)
-                   priceInCartCount++
+                   count.priceInCartCount++
            }
        }
     }
 
-    if (countClearancePrice < 2) {
-        return String.format(
-            "Clearance Price : %d products  @ %.2f\n" +
-                    "Normal Price : %d @ %.2f - %.2f\n" +
-                    "Price in Cart : %d products", countClearancePrice, clearanceHigh,
-            countRegPrice, regularPriceLow, regularPriceHigh, priceInCartCount
-        )
-    } else if (countRegPrice < 2) {
-        return String.format(
-            "Clearance Price : %d products  @ %.2f - %.2f\n" +
-                    "Normal Price : %d @ %.2f\n" +
-                    "Price in Cart : %d products", countClearancePrice, clearanceHigh, clearanceLow,
-            countRegPrice, regularPriceHigh, priceInCartCount
-        )
-    } else {
-        return String.format(
-            "Clearance Price : %d products  @ %.2f - %.2f\n" +
-                    "Normal Price : %d @ %.2f - %.2f\n" +
-                    "Price in Cart : %d products", countClearancePrice, clearanceHigh, clearanceLow,
-            countRegPrice, regularPriceLow, regularPriceHigh, priceInCartCount
-        )
-    }
+   return count
 }
 fun buildObjects(list : List<String>): List<Result> {
 
@@ -124,6 +165,16 @@ data class Result (
     val quantity: Int?,
     val cart : Boolean?
     )
+
+data class Counts (
+    var countRegPrice: Int = 0,
+    var countClearancePrice: Int = 0,
+    var clearanceHigh: Double = 0.00,
+    var clearanceLow: Double = 0.00,
+    var priceInCartCount: Int = 0,
+    var regularPriceHigh: Double = 0.00,
+    var regularPriceLow: Double = 0.00
+        )
 
 enum class Type {
     NORMAL,
